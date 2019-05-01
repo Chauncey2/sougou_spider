3fc76dedfc9bcdff84c7c168fac92e45a12fce17
Chauncey2 <gaochangju95@163.com>
2019-05-01 13:32:30 +0800

    修正bug

--- sougou_spider/pipelines.py
+++ sougou_spider/pipelines.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-
 import re
 
 class SougouSpiderPipeline(object):
-
     def process_item(self, item, spider):
 
         try:
-            if item['answer'][0] != None:
+            if len(item['question']) and len(item['answer']):
                 # 获取回答内容
                 answer_html = item['answer'][0]
                 regex = r'<[^>]+>'
@@ -16,7 +14,7 @@ class SougouSpiderPipeline(object):
                 item['answer'][0] = pattern.sub('', answer_html)
                 print(pattern.sub('', answer_html))
                 return item
+            else:
+                return {'answer': [], 'question': []}
         except Exception as e:
-            print(e)# 打印错误信息
-            raise e # 抛出异常
-        return None
+            print(e.args)  # 打印错误信息
